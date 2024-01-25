03cb261353e5a1b5fde65520a37f4da6cd0fe212
Ian Miller <imiller142@gmail.com>
2024-01-25 10:24:59 -0600

    added database calls

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +9,22 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password1',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// database call
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // default response for any other request (not found)
 
