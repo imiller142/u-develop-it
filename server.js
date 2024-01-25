d84e0015fdd45733d457c040650c7cd1509c0fe4
Ian Miller <imiller142@gmail.com>
2024-01-25 13:12:32 -0600

    More routes

--- server.js
+++ server.js
@@ -21,10 +21,66 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// database call
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// database call get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// get single candidate
+app.get('/api/candidate/:id' , (req, res) => {
+
+    const sql = `SELECT * FROM candidates WHER id = ?`;
+    const params = [req.params.id]
+
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+
+        }
+
+        res.json ({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Delete candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+
+//     console.log(result);
+// });
+
+// Create candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log(result);
+});
 
 // default response for any other request (not found)
 
