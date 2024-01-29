a82a75fb2f4012c73e25fc30c2259d213078c3e4
Ian Miller <imiller142@gmail.com>
2024-01-29 13:55:24 -0600

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//candidates
 router.use(require('./candidateRoutes'));
 
+//parties
 router.use(require('./partyRoutes'));
+
+//voters
+router.use(require('./voterRoutes'));
+
 
 module.exports = router;
